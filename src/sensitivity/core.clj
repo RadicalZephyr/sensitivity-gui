4b126f9005acfc3414a4e10ae1594629c06c083b
Geoff Shannon <earthlingzephyr@gmail.com>
2012-12-30 02:58:39 -0800

    More functions n stuff...

--- src/sensitivity/core.clj
+++ src/sensitivity/core.clj
@@ -1,6 +1,7 @@
 (ns sensitivity.core
   (:use seesaw.core
-        clojure-csv.core))
+        incanter.core)
+  (:require incanter.io))
 
 (defn main-widget []
   "Hello Seesaw")
@@ -14,3 +15,30 @@
        (config! :content (main-widget))
        pack!
        show!)))
+
+
+(defn- load-file [fname]
+  (incanter.io/read-dataset fname :delim \space))
+
+(defn- mean-2d [col]
+  (map /
+       (reduce #(map + %1 %2) col)
+       (repeat (count col))))
+
+(defn- dataset-mean [dataset]
+  (let [cols [:col4 :col5 :col6]]
+    (to-matrix
+     ($ :all cols
+        ($rollup mean-2d cols [] dataset)))))
+
+(defn- do-calculation [func datasets]
+  (map #(func (:negative %)
+              (:positive %)) datasets))
+
+(defn- get-offsets [datasets]
+  (do-calculation plus datasets))
+
+(defn iterate-structure [structure func]
+  (map #(identity {:negative (func (:negative %))
+                   :positive (func (:positive %))})
+       structure))
