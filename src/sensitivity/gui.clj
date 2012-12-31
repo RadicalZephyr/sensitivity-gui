a65afc0fbc2239c0507feb183eeca94079cdb0a8
Geoff Shannon <earthlingzephyr@gmail.com>
2012-12-31 03:28:03 -0800

    Fill out initial gui

--- src/sensitivity/gui.clj
+++ src/sensitivity/gui.clj
@@ -1,7 +1,24 @@
 (ns sensitivity.gui
-  (use sensitivity.core
-       seesaw.core))
+  (use [sensitivity.core :only [root-directory->datasets
+                                get-offsets
+                                get-sensitivities]]
+       seesaw.core
+       [seesaw.chooser :only [choose-file]]))
 
+(defn bind-choose-file [selector]
+  (choose-file
+   :selection-mode :dirs-only
+   :success-fn (fn [fc file]
+                 (config! (select selector)
+                          :text (str (.getAbsolutePath file) "/")))))
+
+(defn dir-select []
+  (horizontal-panel :items
+                    [(text :text "Select a file"
+                           :editable? false
+                           :id :root-dir)
+                     (action :name "..."
+                             :handler #(bind-choose-file [:root-dir]))]))
 
 (defn main-widget []
   "Hello Seesaw")
@@ -11,7 +28,9 @@
   [& args]
   (native!)
   (invoke-later
-   (-> (frame :title "Calculate Sensitivity")
-       (config! :content (main-widget))
+   (-> (frame :title "Calculate Sensitivity"
+              :content (main-widget)
+              :size [400 :by 240])
+       (config! )
        pack!
        show!)))
