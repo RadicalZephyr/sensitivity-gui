3e7d61a9359f9af7f1d3b09e07519a3ff1e3de35
Geoff Shannon <earthlingzephyr@gmail.com>
2012-12-30 23:05:25 -0800

    Make all functions but one private, change buffer size

--- src/sensitivity/io.clj
+++ src/sensitivity/io.clj
@@ -3,30 +3,29 @@
            java.nio.ByteBuffer
            java.nio.ByteOrder))
 
-
-(def BUFFER-SIZE 640)
+(def BUFFER-SIZE 1024)
 
 (def MAGIC-HEADER 0x711ad917)
 
 (def OFFSETS [0x1ff800
               0x1ffc00])
 
-(defn open-channel [file]
+(defn- open-channel [file]
   (.getChannel (RandomAccessFile. file "r")))
 
-(defn channel->bb [channel offset]
+(defn- channel->bb [channel offset]
   (.order
    (.map channel
          java.nio.channels.FileChannel$MapMode/READ_ONLY offset BUFFER-SIZE)
    ByteOrder/LITTLE_ENDIAN))
 
-(defn read-sensor-entry [byte-buffer]
+(defn- read-sensor-entry [byte-buffer]
   (let [timestamp (.getInt byte-buffer)
         accel (apply vector (repeatedly 3 #(.getFloat byte-buffer)))
         gyro  (apply vector (repeatedly 3 #(.getFloat byte-buffer)))]
     [timestamp accel gyro]))
 
-(defn read-data [byte-buffer]
+(defn- read-data [byte-buffer]
   (let [magic-header (.getInt byte-buffer)
         hdrs (doall (repeatedly 4  #(.getInt byte-buffer)))
         sensor-length (.get byte-buffer)]
