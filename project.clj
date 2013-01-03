432e82aeca359311fc7ab391bf8e04c6e067d891
Geoff Shannon <earthlingzephyr@gmail.com>
2013-01-02 22:47:43 -0800

    Add info for lein-launch4j and plugin dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main sensitivity.gui
+  :launch4j-config-file "resources/config.xml"
+  :plugins [[lein-launch4j "0.1.1"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [incanter/incanter-core "1.3.0"]
                  [incanter/incanter-io "1.3.0"]
