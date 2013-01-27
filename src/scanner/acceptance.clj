ff83954216f30309491e959a958613b230f68d6d
Geoff Shannon <earthlingzephyr@gmail.com>
2013-01-27 00:22:15 -0800

    Add lots of comments with road-map and stub out some functions

--- src/scanner/acceptance.clj
+++ src/scanner/acceptance.clj
@@ -6,6 +6,36 @@
         [clojure.string :only [join]]
         [clojure.java.shell :only [sh]]))
 
+
+;; So many thoughts!  OK, so since we're going to do a multiple pass
+;; comparison, maybe we'll need to write out the output of
+;; acceptance.exe to files.  IF SO, put them in target directory,
+;; under target/data/<agman-version>/<scan-name>.csv
+
+;; In reality, keeping the data as incanter datasets is probably best
+;; to avoid disk I/O and such.  We only need to keep two datasets in
+;; memory at once, and just run all the statistical functions over
+;; each.
+
+;; Which brings me to: should think of the compare stage as a
+;; multi-pass operation.  Somewhere, I should be able to define (or
+;; allow the user to) a set of different statistical operations to run
+;; over the data and get the results of each one.
+
+;; Also, for the future a DSL for generating expected outputs of agman
+;; would be nice.  But we'll punt on that one for now.  For now, the
+;; "expected" file will contain a literal line-by-line comparable
+;; dataset to the one produced by the scan.  This will be
+;; hand-generated...
+
+;; Also, I realize now that the independent program is the better
+;; route.  But I need directory structure to take into account that
+;; there could be (and will eventually) multiple versions of agman
+;; under test.  Also, we may potentially want to be able to compare
+;; the metrics to each other over different version of agman.
+(defn list-directories [root]
+  )
+
 (defn write-out-config [root-path offsets sensitivities]
   (spit (str root-path "acceptance.cfg")
         (str
@@ -14,6 +44,9 @@
                 (str "[sensitivity_x] = " (sensitivity->string 0 sensitivities))
                 (str "[sensitivity_y] = " (sensitivity->string 1 sensitivities))
                 (str "[sensitivity_z] = " (sensitivity->string 2 sensitivities))]))))
+
+(defn find-test-cases [root-path]
+  )
 
 (defn -main
   "Run the acceptance test.  Takes a single argument of a folder.
@@ -29,6 +62,17 @@
   The file acceptance.cfg will be re-created every time this runs."
   [root-dir]
   (let [root-path     (validate-root-exists root-dir)
-        {:keys [offsets sensitivities]} (calculate root-dir)]
+        {:keys [offsets sensitivities]} (calculate root-dir)
+        ;; Identify test-case names
+        test-cases (find-test-cases root-path)]
+    ;; Generate a config for this test
     (write-out-config root-path offsets sensitivities)
+
+    ;; Find the right acceptance executable (or executables) and maintain a version to exe mapping
+
+    ;; Run the combinations of version X test-case.  These are the "actual" results
+
+    ;; For each "actual", compare with the "expected"
+
+    ;; Produce output, in some format!
     ))
