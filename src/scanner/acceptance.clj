c8e7a8c635d287f80630e37396c674c6d96986bc
Geoff Shannon <earthlingzephyr@gmail.com>
2013-01-31 22:00:21 -0800

    Add a function to print a dataset to stdout

--- src/scanner/acceptance.clj
+++ src/scanner/acceptance.clj
@@ -80,6 +80,30 @@
                                         sensitivities))])))
     config-file-path))
 
+(defn save-dataset [dataset filename & {:keys [delim header append]
+                            :or {append false delim \,}}]
+  (let [header (or header (map #(if (keyword? %) (name %) %) (:column-names dataset)))
+        file-writer (if (= "-" filename)
+                      *out*
+                      (java.io.FileWriter. filename append))
+        rows (:rows dataset)
+        columns (:column-names dataset)]
+    (do
+      (when (and header (not append))
+        (.write file-writer (str (first header)))
+        (doseq [column-name (rest header)]
+          (.write file-writer (str delim column-name)))
+        (.write file-writer (str \newline)))
+      (doseq [row rows]
+          (do
+            (.write file-writer (str (row (first columns))))
+            (doseq [column-name (rest columns)]
+              (.write file-writer (str delim (row column-name))))
+            (.write file-writer (str \newline))))
+      (.flush file-writer)
+      (.close file-writer))))
+
+
 (defn dir->test-name
   "Expects a java.io.File as directory"
   [directory]
