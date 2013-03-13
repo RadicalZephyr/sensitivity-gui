6ffd831901bf035209d97dd893f8cae2cf94ef93
Geoff Shannon <earthlingzephyr@gmail.com>
2013-03-12 17:01:19 -0700

    Save the results to a file in target as well

--- bin/runtest.sh
+++ bin/runtest.sh
@@ -21,6 +21,7 @@ echo "Setting up..."
 TESTDIR="$1"
 TESTNAME=$(basename $TESTDIR)
 TESTOUTPUT="target/$TESTNAME.csv"
+TESTRES="target/$TESTNAME-results.txt"
 
 echo "Refreshing lein deps"
 lein deps
@@ -29,4 +30,6 @@ echo "Running test..."
 lein run -m scanner.acceptance $TESTDIR > $TESTOUTPUT
 
 echo "Done, analyzing output"
-bin/analyze.r $TESTOUTPUT 2> /dev/null
+bin/analyze.r $TESTOUTPUT 2> /dev/null > $TESTRES
+
+cat $TESTRES
