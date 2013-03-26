2e0c73e041702d7628dc3fac2b1a76b84eb142b7
Geoff Shannon <earthlingzephyr@gmail.com>
2013-03-25 19:39:21 -0700

    Cleanups and add documentation to scanner.io

--- src/scanner/io.clj
+++ src/scanner/io.clj
@@ -3,38 +3,51 @@
            java.nio.ByteBuffer
            java.nio.ByteOrder))
 
-(def BUFFER-SIZE 1024)
+(def ^:const BUFFER-SIZE 1024)
 
-(def FW-MAGIC-HEADER 0x711ad917)
+(def ^:const FW-MAGIC-HEADER
+  "The magic number that should be intact in the header of a valid
+  frame."
+  0x711ad917)
 
-(def CALIBRATION-MAGIC-HEADER 0xAC0FF5E7)
+(def ^:const OFFSETS
+  "The byte offsets at which the two duplicate sensor readings can be
+  found."
+  [0x1ff800 0x1ffc00])
 
-(def OFFSETS [0x1ff800
-              0x1ffc00])
+(defn- open-channel [filename]
+  "Create a channel from a filename."
+  (.getChannel (RandomAccessFile. filename "r")))
 
-(defn- open-channel [file]
-  (.getChannel (RandomAccessFile. file "r")))
-
-(defn- channel->bb [channel offset]
+(defn- channel->bb
+  "Memory map a BUFFER-SIZE amount of memory at offset in the given
+  channel."
+  [channel offset size]
   (.order
    (.map channel
-         java.nio.channels.FileChannel$MapMode/READ_ONLY offset BUFFER-SIZE)
+         java.nio.channels.FileChannel$MapMode/READ_ONLY offset size)
    ByteOrder/LITTLE_ENDIAN))
 
-(defn- read-sensor-entry [byte-buffer]
+(defn- read-sensor-entry
+  "Read a single sample of sensor data."
+  [byte-buffer]
   (let [timestamp (.getInt byte-buffer)
         accel (repeatedly 3 #(.getShort byte-buffer))
         gyro  (repeatedly 3 #(.getShort byte-buffer))]
     (concat [timestamp] gyro accel)))
 
-(defn- read-data [byte-buffer]
+(defn- read-data
+  "Read all of the sensor data available from a byte-buffer."
+  [byte-buffer]
   (let [magic-header (.getInt byte-buffer)
         hdrs (doall (repeatedly 4  #(.getInt byte-buffer)))
         sensor-length (.get byte-buffer)]
     (when (= magic-header FW-MAGIC-HEADER)
       (repeatedly sensor-length #(read-sensor-entry byte-buffer)))))
 
-(defn list-files [directory-name]
+(defn list-files
+  "Get a sorted sequence of all the files in a directory."
+  [directory-name]
   (sort
    (seq
     (.listFiles (java.io.File. directory-name)
@@ -43,7 +56,9 @@
                   (accept [this f]
                     (not (.isDirectory f))))))))
 
-(defn list-directories [directory-name]
+(defn list-directories
+  "Get a sorted sequence of all the sub-directories in a directory."
+  [directory-name]
   (sort
    (seq
     (.listFiles (java.io.File. directory-name)
@@ -52,20 +67,32 @@
                   (accept [this f]
                     (.isDirectory f)))))))
 
-(defn read-data-from-file [file]
+(defn read-data-from-file
+  "Read all of the sensor entries from a file.  Verifies that both
+  offsets contain the same data.  Returns a seq of sample-seqs
+  [timestamp accel_x accel_y accel_z gyro_x gyro_y gyro_z]."
+  [file]
   (with-open [chn (open-channel file)]
-    (let [data (map #(read-data (channel->bb chn %))
+    (let [data (map #(read-data (channel->bb chn % BUFFER-SIZE))
                     OFFSETS)]
       (when (= (first data) (second data))
         (first data)))))
 
-(defn read-data-from-directory [directory]
+(defn read-data-from-directory
+  "Read all of the data for a scan from a directory containing all of
+  the PBMP files.  Returns a larger seq of the same form as
+  read-data-from-file."
+  [directory]
   (mapcat read-data-from-file
           (list-files directory)))
 
 ;; Lifted and modified from incanter.core (incanter.io)
-(defn save-dataset [dataset filename & {:keys [delim header append]
-                            :or {append false delim \,}}]
+(defn save-dataset
+  "Minor modification of the incanter method for saving a data set to
+  allow \"saving\" to std-out by using a \"-\"."
+  [dataset filename &
+   {:keys [delim header append]
+    :or {append false delim \,}}]
   (let [header (or header (map #(if (keyword? %) (name %) %)
                                (:column-names dataset)))
         file-writer (if (= "-" filename)
@@ -80,10 +107,10 @@
           (.write file-writer (str delim column-name)))
         (.write file-writer (str \newline)))
       (doseq [row rows]
-          (do
-            (.write file-writer (str (row (first columns))))
-            (doseq [column-name (rest columns)]
-              (.write file-writer (str delim (row column-name))))
-            (.write file-writer (str \newline))))
+        (do
+          (.write file-writer (str (row (first columns))))
+          (doseq [column-name (rest columns)]
+            (.write file-writer (str delim (row column-name))))
+          (.write file-writer (str \newline))))
       (.flush file-writer)
       (.close file-writer))))
